fe6460a49b15dc43e039693a3d94384dba7669c1
postolenko <denis.postolenko@gmail.com>
2021-01-23 23:46:27 +0200

    Design

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,8 @@
   });
 
 
-  $(".del_tag").on("click", function(e) {
+  // $(".file_tag button").on("click", function(e) {
+  $(document).on("click", ".file_tag button", function(e) {
     e.preventDefault();
     parent = $(this).closest(".file_tag");
     parent.remove();
@@ -40,6 +41,24 @@
     parent = $(this).closest(".message_row");
     parent.remove();
   });
+
+
+  // По одному файлу
+  // document.querySelector('#attach-contact').addEventListener("change", (e) => addFile(e.target.files[0].name));
+
+  // Несколько файлов
+  document.querySelector('#attach-contact-multiple').addEventListener("change", (e) => Array.from(e.target.files).forEach(file => addFile(file.name)));
+
+
+  function addFile(fileName) {
+    let div = document.createElement('div');
+    div.className = "file_tag";
+    div.innerHTML = fileName;
+    let button = document.createElement('button');
+    button.className = "del_tag";
+    div.append(document.createElement('button'));
+    document.querySelector('.files_tags').append(div);
+  }
 
 })(jQuery);
 
