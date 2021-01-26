db193ec18f621505d928f0a89b767c8a9fce6dde
postolenko <denis.postolenko@gmail.com>
2021-01-26 22:51:26 +0200

    Cab

--- scripts/main.js
+++ scripts/main.js
@@ -44,16 +44,55 @@
 
   // ----------
 
-  document.querySelector('#attach-contact-multiple').addEventListener("change", (e) => Array.from(e.target.files).forEach(file => addFile(file.name)));
-  function addFile(fileName) {
-    let div = document.createElement('div');
-    div.className = "file_tag";
-    div.innerHTML = fileName;
-    let button = document.createElement('button');
-    button.className = "del_tag";
-    div.append(document.createElement('button'));
-    document.querySelector('.files_tags').append(div);
+  if($("#attach-contact-multiple").length > 0) {
+    document.querySelector('#attach-contact-multiple').addEventListener("change", (e) => Array.from(e.target.files).forEach(file => addFile(file.name)));
+    function addFile(fileName) {
+      let div = document.createElement('div');
+      div.className = "file_tag";
+      div.innerHTML = fileName;
+      let button = document.createElement('button');
+      button.className = "del_tag";
+      div.append(document.createElement('button'));
+      document.querySelector('.files_tags').append(div);
+    }
   }
+
+  // ----------
+
+  $("[data-dropdown-btn]").on('click', function(e) {
+    e.preventDefault();
+    dropDown = $("[data-dropdown = '"+$(this).attr("data-dropdown-btn")+"']");
+    if(dropDown.is(":hidden")) {
+      dropDown.slideDown(300);
+    } else {
+      dropDown.slideUp(300);
+    }
+  });
+
+  $("[data-ch-single]").change(function() {
+    ch = $(this).attr('data-ch-single');
+    chs = $("[data-ch = '"+ch+"']").find("input");
+    if($(this).is(":checked")) {
+      chs.prop("checked", true);
+    } else {
+      chs.prop("checked", false);
+    }
+  });
+
+  $("[data-ch] input").on("change", function() {
+    parent = $(this).closest("[data-ch]");
+    chChildrens = parent.find("input");
+    chMain = $("[data-ch-single = '"+parent.attr("data-ch")+"']");
+    chChildrens.each(function() {
+      if(!$(this).is(":checked")) {
+        chMain.prop("checked", false);
+        return false;
+      } else {
+        chMain.prop("checked", true);
+      }
+    });
+  });
+
 
 })(jQuery);
 
